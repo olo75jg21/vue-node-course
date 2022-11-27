c2da15cfe91b10d59014777ee4e404496819ce85
Jakub Olcha <olo75jg12@gmail.com>
2022-11-27 21:45:58 +0100

    add ringing view details

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import ViewManager from '@/services/ViewManager'
+// import ViewManager from '@/services/ViewManager'
 import Answered from '../views/AnsweredView.vue'
 import Connected from '../views/ConnectedView.vue'
 import Start from '../views/StartView.vue'
@@ -16,37 +16,37 @@ const routes = [
     name: 'ringing',
     component: Ringing,
     props: true,
-    beforeEnter: (to, from, next) => {
-      if (ViewManager.status !== 'RINGING') {
-        console.log('redirected from ringing');
-        next('/');
-      }
-      next();
-    }
+    // beforeEnter: (to, from, next) => {
+    //   if (ViewManager.status !== 'RINGING') {
+    //     console.log('redirected from ringing');
+    //     next('/');
+    //   }
+    //   next();
+    // }
   },
   {
     path: '/connected',
     name: 'connected',
     component: Connected,
-    beforeEnter: (to, from, next) => {
-      if (ViewManager.status !== 'CONNECTED') {
-        console.log('redirected from connected');
-        next('/');
-      }
-      next();
-    }
+    // beforeEnter: (to, from, next) => {
+    //   if (ViewManager.status !== 'CONNECTED') {
+    //     console.log('redirected from connected');
+    //     next('/');
+    //   }
+    //   next();
+    // }
   },
   {
     path: '/answered',
     name: 'answered',
     component: Answered,
-    beforeEnter: (to, from, next) => {
-      if (ViewManager.status !== 'ANSWERED') {
-        console.log('redirected from answered');
-        next('/');
-      }
-      next();
-    }
+    // beforeEnter: (to, from, next) => {
+    //   if (ViewManager.status !== 'ANSWERED') {
+    //     console.log('redirected from answered');
+    //     next('/');
+    //   }
+    //   next();
+    // }
   }
 ];
 
--- front/src/views/RingingView.vue
+++ front/src/views/RingingView.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="box">
-        <div class="text">
-            Zaraz nastąpi połączenie z konsultantem.
+    <div class="text flex flex-col h-full">
+        <div class="mb-48 mt-4 text-xl">
+            <p>Zaraz nastąpi połączenie.</p>
         </div>
+        <div class="phone flex">
+            <font-awesome-icon class="w-16 h-16" icon="fa-solid fa-phone-volume" />
+        </div>
+
     </div>
 </template>
+
+<script>
+import('../animations/ringingAnimation.css');
+</script>
