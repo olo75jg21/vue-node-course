43ef4cd98e5112aa4ae07a610cefdfee5c2838d4
Jakub Olcha <olo75jg12@gmail.com>
2022-11-27 19:06:38 +0100

    add basic style

---
+++ front/src/components/KeyboardComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="grid grid-cols-3 gap-4 mt-8 mb-8">
+        <button v-for="key in keys" :key="key" :class="btnStyle" @click="press(key)">
+            {{ key }}
+        </button>
+        <button @click="clear()" class="" :class="btnStyle">&larr;</button>
+        <button @click="press(0)" :class="btnStyle">0</button>
+        <button @click="clear('all')" :class="btnStyle">C</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['selfValue'],
+    data() {
+        return {
+            value: '',
+            keys: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            buttonTheme: 'btn-danger',
+            btnStyle: `border-2 border-gray-300 rounded-full h-14 w-14 text-xl text-gray-300 hover:text-white
+                       hover:border-white hover:text-2xl transition duration-150 ease-in-out`
+        };
+    },
+    methods: {
+        press(key) {
+            this.value = `${this.value}${key}`;
+        },
+        clear(type) {
+            if (type === 'all') this.value = '';
+            else this.value = this.value.substring(0, this.value.length - 1);
+        },
+    },
+    watch: {
+        value() {
+            this.$emit('pressed', this.value);
+        },
+        selfValue() {
+            this.value = this.selfValue;
+        },
+    },
+};
+</script>
--- front/src/views/StartView.vue
+++ front/src/views/StartView.vue
@@ -1,12 +1,10 @@
 <template>
     <div>
-        <div class="header clearfix">
-            Zadzwonimy do Ciebie w ciągu 26 sekund.
-        </div>
-        <label class="form-label clearfix" for="form-number">
-            Wprowadź numer
-        </label>
-        <input v-model="number" class="form-number clearfix" id="form-number" />
+        <input type="text" v-model="value" placeholder="Press any button"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+        <KeyboardComponent @pressed="value = $event" :selfValue="value" v-model="number"></KeyboardComponent>
+
         <div class="call-button" @click="call()">
             Zadzwoń teraz
         </div>
@@ -16,10 +14,13 @@
 <script>
 import axios from 'axios';
 import ViewManager from '../services/ViewManager';
+import KeyboardComponent from '@/components/KeyboardComponent.vue';
 
 export default {
+    components: { KeyboardComponent },
     data() {
         return {
+            value: '',
             number: ''
         }
     },
@@ -27,7 +28,7 @@ export default {
     methods: {
         async call() {
             try {
-                const { data } = await axios.post('call', { number: this.number })
+                const { data } = await axios.post('call', { number: this.value })
                 ViewManager.checkStatus()
                 this.$router.push({ name: 'ringing', params: { callsId: data.id } })
             } catch (e) {
