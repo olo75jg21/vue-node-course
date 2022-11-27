9cfb8ad787fd94d8b54b402ead3f1722ecc921f1
Jakub Olcha <olo75jg12@gmail.com>
2022-11-27 22:47:07 +0100

    answereed view style

--- front/src/views/AnsweredView.vue
+++ front/src/views/AnsweredView.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <div>
-            Połączenie zakończone.
-        </div>
-        <div @click="$router.push('/')">
+        <p class="text-2xl mt-4 mb-40">Połączenie zakończone</p>
+        <div @click="$router.push('/')"
+            class="text-center text-md text- cursor-pointer border-2 border-zinc-800 rounded-md border-white bg-zinc-500 mx-auto w-40 duration-75 hover:rounded-full hover:scale-110 hover:text-white hover:border-white">
             Spróbuj ponownie
         </div>
     </div>
