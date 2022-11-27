a1dd434b8e1eab81b49cd27206a814498ad835c8
Jakub Olcha <olo75jg12@gmail.com>
2022-11-27 22:59:39 +0100

    add failed view

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-// import ViewManager from '@/services/ViewManager'
-import Answered from '../views/AnsweredView.vue'
-import Connected from '../views/ConnectedView.vue'
-import Start from '../views/StartView.vue'
-import Ringing from '../views/RingingView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import ViewManager from '@/services/ViewManager'
+import Answered from '../views/AnsweredView.vue';
+import Connected from '../views/ConnectedView.vue';
+import Failed from '../views/FailedView.vue';
+import Ringing from '../views/RingingView.vue';
+import Start from '../views/StartView.vue';
 
 const routes = [
   {
@@ -16,37 +17,45 @@ const routes = [
     name: 'ringing',
     component: Ringing,
     props: true,
-    // beforeEnter: (to, from, next) => {
-    //   if (ViewManager.status !== 'RINGING') {
-    //     console.log('redirected from ringing');
-    //     next('/');
-    //   }
-    //   next();
-    // }
+    beforeEnter: (to, from, next) => {
+      if (ViewManager.status !== 'RINGING') {
+        next('/');
+      }
+      next();
+    }
   },
   {
     path: '/connected',
     name: 'connected',
     component: Connected,
-    // beforeEnter: (to, from, next) => {
-    //   if (ViewManager.status !== 'CONNECTED') {
-    //     console.log('redirected from connected');
-    //     next('/');
-    //   }
-    //   next();
-    // }
+    beforeEnter: (to, from, next) => {
+      if (ViewManager.status !== 'CONNECTED') {
+        next('/');
+      }
+      next();
+    }
   },
   {
     path: '/answered',
     name: 'answered',
     component: Answered,
-    // beforeEnter: (to, from, next) => {
-    //   if (ViewManager.status !== 'ANSWERED') {
-    //     console.log('redirected from answered');
-    //     next('/');
-    //   }
-    //   next();
-    // }
+    beforeEnter: (to, from, next) => {
+      if (ViewManager.status !== 'ANSWERED') {
+        next('/');
+      }
+      next();
+    }
+  },
+  {
+    path: '/failed',
+    name: 'failed',
+    component: Failed,
+    beforeEnter: (to, from, next) => {
+      if (ViewManager.status !== 'FAILED') {
+        next('/');
+      }
+      next();
+    }
   }
 ];
 
---
+++ front/src/views/FailedView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p class="text-2xl mt-4 mb-40">Połączenie nieudane</p>
+        <div @click="$router.push('/')"
+            class="text-center text-md text- cursor-pointer border-2 border-zinc-800 rounded-md border-white bg-zinc-500 mx-auto w-40 duration-75 hover:rounded-full hover:scale-110 hover:text-white hover:border-white">
+            Spróbuj ponownie
+        </div>
+    </div>
+</template>
