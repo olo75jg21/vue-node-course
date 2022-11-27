0de3f315722a5043cebaa724880fc29fef4d4495
Jakub Olcha <olo75jg12@gmail.com>
2022-11-27 16:40:25 +0100

    add tailwind

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="window">
-    <router-view/>
-</div>
+    <router-view />
+  </div>
 </template>
 
 <style>
-
 .window {
   width: 800px;
   height: 500px;
@@ -23,11 +22,13 @@
   font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
     "Lucida Sans", Arial, sans-serif;
 }
+
 .clearfix:after {
   content: "";
   clear: both;
   display: table;
 }
+
 .close {
   position: absolute;
   right: 10px;
@@ -36,27 +37,33 @@
   cursor: pointer;
   font-size: 1.15em;
 }
+
 .box {
   text-align: center;
 }
+
 .text {
   font-size: 40px;
   font-weight: bold;
   margin-top: 30px;
 }
+
 .header,
 .form-number {
   margin-bottom: 50px;
 }
+
 .header {
   margin-top: 50px;
   font-size: 38px;
   font-weight: 600;
 }
+
 .form-label {
   font-size: 32px;
   font-weight: 600;
 }
+
 .form-number {
   width: 400px;
   height: 55px;
@@ -66,6 +73,7 @@
   letter-spacing: 2px;
   text-align: center;
 }
+
 .call-button,
 .call-button-bottom {
   width: 300px;
@@ -84,13 +92,13 @@
 .call-button-bottom {
   margin-top: 300px;
 }
+
 .call-button:hover {
   filter: brightness(80%);
 }
+
 .error {
   border: 1px solid #ff0000;
   background: rgb(255, 170, 170);
 }
-
-
 </style>
